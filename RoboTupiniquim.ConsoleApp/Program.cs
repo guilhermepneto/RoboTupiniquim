0da2d129c63761204910ce72e0a78a2cddad2980
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-04-28 21:16:52 -0300

    Add input de informaçoes iniciais

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Robo Tupiniquim
+
+//1ª parte - Input de coordenadas e direção
+
+Console.Clear();
+Console.WriteLine("Informe as coordenadas X e Y, respectivamente, e a direção que o robô está olhando (N,S,L,O)");
+string coord_x = Console.ReadLine();
+string coord_y = Console.ReadLine();
+string direcao = Console.ReadLine();
+
+Console.WriteLine("A posição inicial do robô é: " + coord_x + " " + coord_y + " " + direcao);
+Console.WriteLine("Pressione ENTER para continuar");
+
+
+
+
+
+return;
