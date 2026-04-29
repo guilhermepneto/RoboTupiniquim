24f084c3a8122a03aecbb4bea0770677a9ae5ee0
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-04-28 23:25:04 -0300

    Add execução de sequencia de comandos

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,18 +1,18 @@
 ﻿//Robo Tupiniquim
 
-//1ª parte - Input de coordenadas e direção
-
 while (true)
 {
+    //1ª parte - Input de coordenadas e direção
+
     Console.Clear();
     Console.WriteLine("Informe as coordenadas X e Y, respectivamente, e a direção que o robô está olhando (N,S,L,O)");
     string? inputInicial = Console.ReadLine();
 
     string[] dados = inputInicial.Split(' ');
 
-    string coord_x = dados[0];
-    string coord_y = dados[1];
-    string direcao = dados[2];
+    int coord_x = Convert.ToInt32(dados[0]);
+    int coord_y = Convert.ToInt32(dados[1]);
+    string direcao = dados[2].ToUpper();
 
     Console.WriteLine("A posição inicial do robô é: " + coord_x + " " + coord_y + " " + direcao);
     Console.WriteLine("Pressione ENTER para continuar");
@@ -20,7 +20,7 @@ while (true)
 
     //2ª parte - Input de sequencia de comandos
 
-    Console.WriteLine("Informe a sequência de comandos que o robô irá percorrer no grid");
+    Console.WriteLine("Informe a sequência de comandos que o robô irá percorrer no grid (E,D ou M)");
     string sequenciaComandos = Console.ReadLine();
 
     char[] instrucoes = sequenciaComandos.ToCharArray();
@@ -28,23 +28,83 @@ while (true)
     Console.WriteLine("Pressione ENTER para continuar");
     Console.ReadLine();
 
+    //3ª parte - Execução da sequência de comandos
 
     foreach (char comando in instrucoes)
     {
-        if (comando == 'M')
+        switch (comando)
         {
-            Console.WriteLine("O robô moveu para frente");
-        }
-        else if (comando == 'E')
-        {
-            Console.WriteLine("O robô virou para a esquerda");
-        }
-        else if (comando == 'D')
-        {
-            Console.WriteLine("O robô virou para a direita");
+            case 'M':
+                switch (direcao)
+                {
+                    case "N":
+                        coord_y++;
+                        break;
+
+                    case "S":
+                        coord_y--;
+                        break;
+
+                    case "L":
+                        coord_x++;
+                        break;
+
+                    case "O":
+                        coord_x ==;
+                        break;
+                }
+                break;
+
+            case 'E':
+                switch (direcao)
+                {
+                    case "N":
+                        direcao = "O";
+                        break;
+
+                    case "O":
+                        direcao = "S";
+                        break;
+
+                    case "S":
+                        direcao = "L";
+                        break;
+
+                    case "L":
+                        direcao = "N";
+                        break;
+                }
+                break;
+
+            case 'D':
+                switch (direcao)
+                {
+                    case "N":
+                        direcao = "L";
+                        break;
+
+                    case "O":
+                        direcao = "N";
+                        break;
+
+                    case "S":
+                        direcao = "O";
+                        break;
+
+                    case "L":
+                        direcao = "S";
+                        break;
+                }
+                break;
         }
     }
-    return;
+    Console.WriteLine("A posição final do robô é: " + coord_x + " " + coord_y + " " + direcao);
+
+    Console.WriteLine("Deseja executar o programa novamente? (s/N)");
+    string opcaocontinuar = Console.ReadLine().ToUpper();
+
+    if (opcaocontinuar != "S")
+        break;
 
 }
 
